f40a8ce2cb4b8d8b86febd6f080ce7abdb38b22d
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2023-09-09 02:46:19 -0400

    fix reversed string exercise

--- 01-basic-challenges-1/07-reverse-string/reverse-string.js
+++ 01-basic-challenges-1/07-reverse-string/reverse-string.js
@@ -1,3 +1,7 @@
-function reverseString() {}
+function reverseString(str) {
+  let reversedString = [];
+  str.split("").map((letter) => reversedString.unshift(letter));
+  return reversedString.join("");
+}
 
 module.exports = reverseString;
