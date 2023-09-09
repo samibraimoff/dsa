116044b95902768459bf76e3fa753eb0bca25576
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2023-09-09 02:04:56 -0400

    fix title case exercise

--- 01-basic-challenges-1/06-title-case/title-case.js
+++ 01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,14 @@
-function titleCase() {}
+function titleCase(str) {
+  let firstLetter;
+  let lastLetters;
+  return str
+    .split(" ")
+    .map((word) => {
+      firstLetter = word.charAt(0).toUpperCase();
+      lastLetters = word.slice(1, word.length).toLowerCase();
+      return firstLetter + lastLetters;
+    })
+    .join(" ");
+}
 
 module.exports = titleCase;
