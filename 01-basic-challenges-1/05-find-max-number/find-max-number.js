b229814b6d9de39cfb3330e85247dc92bf395ebd
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2023-09-09 01:27:11 -0400

    solve findMaxNum exercise

--- 01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ 01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,3 +1,11 @@
-function findMaxNumber() {}
+function findMaxNumber(array) {
+  let maxNumber = 0;
+  for (let number of array) {
+    if (number > maxNumber) {
+      maxNumber = number;
+    }
+  }
+  return maxNumber;
+}
 
 module.exports = findMaxNumber;
