c3fcbd8b908cdd04ec2501d409bc891413cffc33
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2023-09-13 21:48:07 -0400

    count vowels exercise

--- 01-basic-challenges-1/09-count-vowels/count-vowels.js
+++ 01-basic-challenges-1/09-count-vowels/count-vowels.js
@@ -1,3 +1,23 @@
-function countVowels() {}
+function countVowels(str) {
+  //   const vowels = ["a", "e", "i", "o", "u"];
+  let countVowels = 0;
+  const formattedStr = str.toLowerCase();
+
+  for (let i = 0; i < formattedStr.length; i++) {
+    let char = formattedStr[i];
+
+    if (
+      char === "a" ||
+      char === "i" ||
+      char === "o" ||
+      char === "e" ||
+      char === "u"
+    ) {
+      countVowels++;
+    }
+  }
+
+  return countVowels;
+}
 
 module.exports = countVowels;
