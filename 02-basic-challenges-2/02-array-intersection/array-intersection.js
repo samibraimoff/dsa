b7d33d62fe598d04940d9e6cba5e529d1136dc08
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2023-09-15 00:31:06 -0400

    exercise 02 array intersection

--- 02-basic-challenges-2/02-array-intersection/array-intersection.js
+++ 02-basic-challenges-2/02-array-intersection/array-intersection.js
@@ -1,3 +1,12 @@
-function arrayIntersection() {}
+function arrayIntersection(arr1, arr2) {
+  const arr = [];
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr2.includes(arr1[i]) && !arr.includes(arr[i])) {
+      arr.push(arr1[i]);
+    }
+  }
+
+  return arr;
+}
 
 module.exports = arrayIntersection;
