f5279938f69595ca58071f8c6e3319f9195b7612
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2023-09-15 00:18:28 -0400

    exercise 01-fizzbuzz

--- 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
+++ 02-basic-challenges-2/01-fizzbuzz-array/fizzbuzz-array.js
@@ -1,3 +1,19 @@
-function fizzBuzzArray() {}
+function fizzBuzzArray(num) {
+  const arr = [];
+
+  for (let i = 1; i <= num; i++) {
+    const str =
+      i % 3 === 0 && i % 5 === 0
+        ? "FizzBuzz"
+        : i % 3 === 0
+        ? "Fizz"
+        : i % 5 === 0
+        ? "Buzz"
+        : i;
+
+    arr.push(str);
+  }
+  return arr;
+}
 
 module.exports = fizzBuzzArray;
