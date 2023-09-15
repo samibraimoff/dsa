089b2bab895de58e2a84ec2561d408b6765f26b0
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2023-09-15 00:53:37 -0400

    exercise 03 display likes

--- 02-basic-challenges-2/03-display-likes/display-likes.js
+++ 02-basic-challenges-2/03-display-likes/display-likes.js
@@ -1,3 +1,19 @@
-function displayLikes() {}
+function displayLikes(likes) {
+  if (!likes.length) return "no one likes this";
+
+  for (let i = 0; i < likes.length; i++) {
+    if (likes.length === 1) return `${likes[i]} likes this`;
+
+    if (likes.length === 2) return `${likes[0]} and ${likes[1]} like this`;
+
+    if (likes.length === 3)
+      return `${likes[0]}, ${likes[1]} and ${likes[2]} like this`;
+
+    if (likes.length > 3)
+      return `${likes[0]}, ${likes[1]} and ${
+        likes.length - 2
+      } others like this`;
+  }
+}
 
 module.exports = displayLikes;
