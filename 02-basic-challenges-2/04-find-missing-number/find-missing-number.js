be913cb55789b253620631b91de171fcaaec0bc7
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2023-09-15 01:16:58 -0400

    exercise 04-find missing number

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -1,3 +1,13 @@
-function findMissingNumber() {}
+function findMissingNumber(arr) {
+  const n = Math.max(...arr);
+  let sumOfTotal = (n * (n + 1)) / 2;
+  let sumOfNumbers = 0;
+
+  for (i = 0; i < arr.length; i++) {
+    sumOfNumbers = sumOfNumbers + arr[i];
+  }
+
+  return sumOfTotal - sumOfNumbers;
+}
 
 module.exports = findMissingNumber;
