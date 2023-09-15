35e04fd2358ef69d4045b84f97d927df2ac38ffe
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2023-09-14 23:53:45 -0400

    exercise 10 remove duplicate

--- 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
+++ 01-basic-challenges-1/10-remove-duplicates/remove-duplicates.js
@@ -1,3 +1,13 @@
-function removeDuplicates() {}
+function removeDuplicates(arr) {
+  const withoutDuplicates = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!withoutDuplicates.includes(arr[i])) {
+      withoutDuplicates.push(arr[i]);
+    }
+  }
+
+  return withoutDuplicates;
+}
 
 module.exports = removeDuplicates;
