afbf2d812a975c0ef598d181071a162ac928a87c
Sam Ibraimoff <sam.ibraimoff@gmail.com>
2023-09-15 01:18:50 -0400

    exercise 04 find missing number, add undefined case

--- 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
+++ 02-basic-challenges-2/04-find-missing-number/find-missing-number.js
@@ -3,6 +3,8 @@ function findMissingNumber(arr) {
   let sumOfTotal = (n * (n + 1)) / 2;
   let sumOfNumbers = 0;
 
+  if (!arr || arr.length === 0) return undefined;
+
   for (i = 0; i < arr.length; i++) {
     sumOfNumbers = sumOfNumbers + arr[i];
   }
